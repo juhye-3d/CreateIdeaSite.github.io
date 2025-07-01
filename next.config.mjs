d3af1bdc82282fb27f6fa72cdef9743ad31e091a
2Jay <70768127+juhye-3d@users.noreply.github.com>
2025-07-01 14:41:49 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export', // ✅ 정적 사이트로 내보내기
+  basePath: '/CreateIdeaSite.github.io', // ✅ GitHub Pages 하위 경로 맞춤
 }
 
 export default nextConfig
